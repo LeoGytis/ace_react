1de7d0a8f77ca4596051f4a7843e9dd544dc04ae
leogytis <leogytis@gmail.com>
2024-09-28 11:44:01 +0300

    build dar neveikia pilnai

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import LandingPageFeatures from './components/LandingPageFeatures';
 // ${inter.className}
 function App() {
 	return (
-		<body className={` antialiased w-full flex flex-col items-center justify-center bg-white`}>
+		<div className={` antialiased w-full flex flex-col items-center justify-center bg-white`}>
 			<main className="flex flex-col justify-center w-full text-3xl font-semibold tracking-tight whitespace-nowrap bg-white bg-opacity-90 max-w-[1440px] text-neutral-800 max-md:max-w-full">
 				<LandingPageHeader />
 				<ArchitectCardsContainer />
@@ -21,7 +21,7 @@ function App() {
 				<LandingPageFeatures />
 			</main>
 			<LandingPageFooter />
-		</body>
+		</div>
 	);
 }
 
