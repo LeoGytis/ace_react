f135a98d051528536170b029e4301263f1e184c7
leogytis <leogytis@gmail.com>
2024-10-17 10:47:55 +0300

    svg import

--- src/components/LandingPageFeatures.tsx
+++ src/components/LandingPageFeatures.tsx
@@ -1,65 +1,52 @@
 import Image from 'next/image';
-import ArrowsIcon from '@/assets/icons/ArrowsFeature.svg?url';
-import BulbIcon from '@/assets/icons/BulbFeature.svg?url';
-import PersonIcon from '@/assets/icons/PersonFeature.svg?url';
+import ArrowsIcon from '../icons/ArrowsFeature.svg';
+import BulbIcon from '../icons/BulbFeature.svg';
+import PersonIcon from '../icons/PersonFeature.svg';
 
 interface Feature {
-  icon: string;
-  title: string;
-  description: string;
+	icon: string;
+	title: string;
+	description: string;
 }
 
 const features: Feature[] = [
-  {
-    icon: ArrowsIcon,
-    title: 'Tailored to the AEC industry',
-    description:
-      'This is not a general platform – Acertip is specifically designed for architects, engineers, and constructors, providing all the necessary tools to work and grow more efficiently.',
-  },
-  {
-    icon: BulbIcon,
-    title: 'Knowledge and project base.',
-    description:
-      'Share experiences, learn from others, showcase your projects, and receive valuable feedback and recognition.',
-  },
-  {
-    icon: PersonIcon,
-    title: 'Targeted talent search.',
-    description:
-      'Companies can quickly find the right specialists by filtering through specific skills and project experience.',
-  },
+	{
+		icon: ArrowsIcon,
+		title: 'Tailored to the AEC industry',
+		description:
+			'This is not a general platform – Acertip is specifically designed for architects, engineers, and constructors, providing all the necessary tools to work and grow more efficiently.',
+	},
+	{
+		icon: BulbIcon,
+		title: 'Knowledge and project base.',
+		description:
+			'Share experiences, learn from others, showcase your projects, and receive valuable feedback and recognition.',
+	},
+	{
+		icon: PersonIcon,
+		title: 'Targeted talent search.',
+		description:
+			'Companies can quickly find the right specialists by filtering through specific skills and project experience.',
+	},
 ];
 
 const LandingPageFeatures = () => {
-  return (
-    <section className="w-full flex flex-col justify-center items-center p-20 lg:p-40">
-      <h2 className="text-7xl font-semibold text-center text-wrap text-neutral-800 mb-20">
-        What Sets Acertip Apart?
-      </h2>
-      <div className="flex flex-col lg:flex-row gap-10 items-start">
-        {features.map((feature, index) => (
-          <article
-            key={index}
-            className="flex flex-col flex-1 justify-center p-4 "
-          >
-            <Image
-              src={feature.icon}
-              alt="feature_icon"
-              width={48}
-              height={48}
-              className="mb-2"
-            />
-            <h3 className="text-xl font-semibold text-neutral-800">
-              {feature.title}
-            </h3>
-            <p className="text-wrap text-base font-normal text-slate-600">
-              {feature.description}
-            </p>
-          </article>
-        ))}
-      </div>
-    </section>
-  );
+	return (
+		<section className="w-full flex flex-col justify-center items-center p-20 lg:p-40">
+			<h2 className="text-7xl font-semibold text-center text-wrap text-neutral-800 mb-20">
+				What Sets Acertip Apart?
+			</h2>
+			<div className="flex flex-col lg:flex-row gap-10 items-start">
+				{features.map((feature, index) => (
+					<article key={index} className="flex flex-col flex-1 justify-center p-4 ">
+						<Image src={feature.icon} alt="feature_icon" width={48} height={48} className="mb-2" />
+						<h3 className="text-xl font-semibold text-neutral-800">{feature.title}</h3>
+						<p className="text-wrap text-base font-normal text-slate-600">{feature.description}</p>
+					</article>
+				))}
+			</div>
+		</section>
+	);
 };
 
 export default LandingPageFeatures;
--- src/components/LandingPageHeader.tsx
+++ src/components/LandingPageHeader.tsx
@@ -1,29 +1,23 @@
 import Image from 'next/image';
-import LoginModal from './LoginModal';
-import RegisterModal from './RegisterModal';
 
 export const LandingPageHeader = () => {
-  return (
-    <header className="flex flex-col gap-4 items-center w-full max-md:px-5 max-md:py-4 px-8 py-2.5 pb-24">
-      <div className="w-full flex justify-between items-center mb-16">
-        <Image src="/logo.svg" alt="Logo" width={101} height={22} />
-        <div className="flex gap-4 items-center text-sm font-medium">
-          <LoginModal />
-          <RegisterModal />
-        </div>
-      </div>
-      <div className=" px-4 py-2 text-sm font-medium leading-loose text-center bg-yellow-200 border border-solid border-black border-opacity-10 rounded-full">
-        We are launching this winter!
-        <span className="text-neutral-800">🚀 </span>
-      </div>
-      <div className="mt-8 text-7xl font-semibold tracking-tighter text-center leading-[72px] text-neutral-900 max-md:max-w-full max-md:text-4xl max-md:leading-10">
-        <h1 className="text-wrap">
-          Connect, collaborate, share, learn and grow with architects,
-          engineers, and constructors.
-        </h1>
-      </div>
-    </header>
-  );
+	return (
+		<header className="flex flex-col gap-4 items-center w-full max-md:px-5 max-md:py-4 px-8 py-2.5 pb-24">
+			<div className="w-full flex justify-between items-center mb-16">
+				<Image src="/logo.svg" alt="Logo" width={101} height={22} />
+				<div className="flex gap-4 items-center text-sm font-medium"></div>
+			</div>
+			<div className=" px-4 py-2 text-sm font-medium leading-loose text-center bg-yellow-200 border border-solid border-black border-opacity-10 rounded-full">
+				We are launching this winter!
+				<span className="text-neutral-800">🚀 </span>
+			</div>
+			<div className="mt-8 text-7xl font-semibold tracking-tighter text-center leading-[72px] text-neutral-900 max-md:max-w-full max-md:text-4xl max-md:leading-10">
+				<h1 className="text-wrap">
+					Connect, collaborate, share, learn and grow with architects, engineers, and constructors.
+				</h1>
+			</div>
+		</header>
+	);
 };
 
 export default LandingPageHeader;
